4c746c3f109a33bdca40d77f4763bbcee2c58b32
Venk Tatineni <venk.tatineni@ticketmaster.com>
2018-01-18 09:50:07 -0800

    jpa

--- src/main/java/slack/app/tiny/model/Link.java
+++ src/main/java/slack/app/tiny/model/Link.java
@@ -1,3 +1,67 @@
 package slack.app.tiny.model;
+
+import com.sun.istack.internal.Nullable;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name    = "link",
+        indexes = {@Index(name = "url_lookup", columnList = "url")})
 public class Link {
+
+    @GeneratedValue
+    @Id
+    private int id;
+    @Column (nullable = false)
+    private String url;
+    @Column
+    private String code;
+    @Column (nullable = false)
+    private Date created;
+    @Column (nullable = false)
+    private Date updated;
+
+    private static final String PERSISTENCE_UNIT_NAME = "short_links";
+    private static EntityManagerFactory factory;
+
+
+    public Link(String $url){
+        this.url = url;
+    }
+
+    public String getUrl( ) {
+        return url;
+    }
+
+    public int getId(){return id;}
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public int createRow(){
+        EntityManager em = getEntityManager();
+        em.getTransaction().begin();
+        em.persist(this);
+        em.getTransaction().commit();
+        em.flush();
+        return this.getId();
+    }
+
+    @PrePersist
+    protected void onCreate() {
+        created = new Date();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updated = new Date();
+    }
+
+    private EntityManager getEntityManager(){
+        factory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
+        EntityManager em = factory.createEntityManager();
+        return em;
+    }
 }
